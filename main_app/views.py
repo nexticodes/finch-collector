1888de43aa6e3e8399c540e1be1e730e8d68b302
Ernest Bruno <erndobruno@gmail.com>
2022-01-10 16:36:59 -0800

    Add finches_index method that retrieves all finches in db and passes it to template.

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
 from django.http import HttpResponse
@@ -8,3 +9,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finches': finches})
