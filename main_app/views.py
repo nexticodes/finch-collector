7b42a4e6a3d2f11fd2a39b20f3bacc8224c4075d
Ernest Bruno <erndobruno@gmail.com>
2022-01-11 20:15:47 -0800

    Add view method for add_feeding that processes the form submitted by user.

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from .models import Finch
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-# Create your views here.
-from django.http import HttpResponse
+from .models import Finch
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -17,7 +15,19 @@ def finches_index(request):
 
 def finches_details(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/details.html', {'finch': finch})
+    feeding_form = FeedingForm()
+    return render(request, 'finches/details.html', {
+        'finch': finch,
+        'feeding_form': feeding_form
+        })
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit = False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('finches_detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
